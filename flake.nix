053b3c722d196ccb0bcdd9b50d8e54c57e9b3c88
J <2742259+javdl@users.noreply.github.com>
2025-01-23 08:06:19 +0100

    nix flake for WASM

---
+++ flake.nix
@@ -0,0 +1,68 @@
+# SPDX-FileCopyrightText: 2025 Joost van der Laan <joost@fashionunited.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+{
+  description = "Development environment for redirect";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, crane, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        
+        # Latest stable Rust
+        rustToolchain = pkgs.rust-bin.stable."1.75.0".default.override {
+          extensions = [ "rust-src" "rust-analyzer" "clippy" ];
+          targets = [ "wasm32-wasi" ];
+        };
+
+        # this is how we can tell crane to use our toolchain!
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        # cf. https://crane.dev/API.html#libcleancargosource
+        src = craneLib.cleanCargoSource ./api;
+        # as before
+        nativeBuildInputs = with pkgs; [ rustToolchain pkg-config ];
+        buildInputs = with pkgs; [ openssl fontconfig ];
+        # because we'll use it for both `cargoArtifacts` and `bin`
+        commonArgs = {
+          inherit src buildInputs nativeBuildInputs;
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        # remember, `set1 // set2` does a shallow merge:
+        bin = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
+      in
+      {
+         packages =
+            {
+              # that way we can build `bin` specifically,
+              # but it's also the default.
+              inherit bin;
+              default = bin;
+            };
+
+        devShells.default = pkgs.mkShell {
+          # instead of passing `buildInputs` / `nativeBuildInputs`,
+            # we refer to an existing derivation here
+            inputsFrom = [ bin ];
+            buildInputs = with pkgs; [ reuse clippy-sarif sarif-fmt sqlite sqlx-cli ];
+
+          # Environment variables
+          shellHook = ''
+            echo "🦀 Welcome to the redirect development environment!"
+          '';
+        };
+      }
+    );
+}
